b687fb4c171a716ff03a7645ec9625200715cef4
hong2goon <hong2goon@gmail.com>
2021-03-16 18:32:49 +0900

    first edit

--- src/components/TodoPage.vue
+++ src/components/TodoPage.vue
@@ -1,29 +1,24 @@
 <template>
   <div class="container">
     <h2>Todo List</h2>
-    <div class="input-group" style="margin-bottom:10px;">
-      <input type="text" class="form-control"
-        placeholder="할 일을 입력하세요"
-        v-model="name"
-        v-on:keyup.enter="createTodo(name)"
-      >
-      <span class="input-group-btn">
-        <button class="btn btn-default" type="button" @click="createTodo(name)">추가</button>
-      </span>
-    </div>
+    
     <ul class="list-group">
       <li class="list-group-item" v-for="(todo, index) in todos">
-        {{ todo.name }}
-        <div class="btn-group pull-right" style="font-size:12px; line-height:1;">
-          <button type="button" class="btn-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            더보기<span class="caret"></span>
-          </button>
-          <ul class="dropdown-menu">
-            <li><a href="#" @click="deleteTodo(index)">삭제</a></li>
-          </ul>
-        </div>
+        <span>{{ todo.context }}</span>
+        <button type="button" class="btn-del" @click="deleteTodo(index)">삭제</button>
       </li>
     </ul>
+
+    <div class="input-group">
+      <input type="text" class="form-control"
+        placeholder="할 일을 입력하세요"
+        v-model="context"
+        v-on:keyup.enter="createTodo(context)"
+      >
+      <span class="input-group-btn">
+        <button class="btn btn-default" type="button" @click="createTodo(context)">추가</button>
+      </span>
+    </div>
   </div>
 </template>
 
@@ -32,20 +27,45 @@
     name: 'TodoPage',
     data() {
       return {
-        name:null,
-			  todos: [{name:'청소'},{name:'블로그 쓰기'},{name:'밥먹기'},{name:'안녕'}]
+        context:null,
+			  todos: [{context:'청소'},{context:'블로그 쓰기'},{context:'밥먹기'},{context:'안녕'}]
 		  }
     },
     methods: {
       deleteTodo(i) {
         this.todos.splice(i, 1)
       },
-      createTodo(name) {
-        if(name != null){
-          this.todos.push({name: name})
-          this.name = null
+      createTodo(context) {
+        if(context != null){
+          this.todos.push({context: context})
+          this.context = null
         }
       }
     }
   }
 </script>
+
+<style>
+  .container {
+    display:flex;
+    flex-direction:column;
+    flex-wrap:wrap;
+    margin:0 auto;
+    padding:0;
+    max-width:768px;
+    height:100%;
+    border:1px solid #eee;
+    border-radius:4px;
+  }
+  h2 {flex:none; margin:0; padding:20px 15px; border-bottom:1px solid #eee;}
+  .list-group {flex:1; overflow:auto; margin:0; padding:0; list-style:none;}
+  .list-group-item {position:relative; padding:15px; border-bottom:1px solid #ddd;}
+  .btn-del {position:absolute; top:50%; right:15px; width:20px; height:20px; text-indent:-9999px; background:transparent; border:none; opacity:0.5; overflow:hidden; transform:translateY(-50%); transition:opacity .5s;}
+  .btn-del:hover {opacity:1;}
+  .btn-del:before, .btn-del:after {content:''; display:block; position:absolute; top:50%; left:50%; width:2px; height:20px; background:#f00; transform-origin:center;}
+  .btn-del:before {transform:translate(-50%, -50%) rotate(45deg);}
+  .btn-del:after {transform:translate(-50%, -50%) rotate(-45deg);}
+  .input-group {flex:none; padding:10px; width:100%; background:#efefef; border-radius:0 0 4px 4px;}
+  .form-control {padding:5px; width:calc(100% - 80px); line-height:2; border:1px solid #ddd;}
+  .btn {padding:5px; width:74px; font-weight:bold; line-height:2; background:#fefefe; border:1px solid #eee;}
+</style>
